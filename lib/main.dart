dca47cb92710cb813d7bae0fa03ddc14fc1631a1
Tung Nguyen <nguyenmanhtung848@gmail.com>
2020-08-19 09:03:40 +0700

    refactoring, new wiget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './answer.dart';
-import './question.dart';
+import 'package:learn_flutter/quiz.dart';
+import 'result.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  final questions = [
+  final _questions = [
     {
       'questionText': 'What is your name',
       'answers': ['Tung', 'Bim']
@@ -44,21 +44,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(questions[_questionIndex]['questionText']),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) => Answer(
-                            selectHandler: _answerQuestion,
-                            answerText: answer,
-                          ))
-                      .toList()
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
               )
-            : Center(
-                child: Text('You did it!'),
-              ),
+            : Result(),
       ),
     );
   }
