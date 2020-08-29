87126c4bb05b739c73bec014f156004c642f0e99
Tung Nguyen <nguyenmanhtung848@gmail.com>
2020-08-29 10:52:08 +0700

    dummy meals

--- lib/screen/category_meals_screen.dart
+++ lib/screen/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class CategoryMealScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   // final String categoryId;
@@ -12,14 +14,21 @@ class CategoryMealScreen extends StatelessWidget {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
-    final id = routeArgs['id'];
+    final cateoryId = routeArgs['id'];
 
+    final cateogryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(cateoryId))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('Recipes for category'),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final currentCategory = cateogryMeals[index];
+          return Text(currentCategory.title);
+        },
+        itemCount: cateogryMeals.length,
       ),
     );
   }
