4564d7a5131b033bd82e84c54c2c637718d08199
Tung Nguyen <nguyenmanhtung848@gmail.com>
2020-09-09 08:08:34 +0700

    auto logout

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -8,7 +9,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
-
+  Timer _authTimer;
   bool get isAuthenticated {
     return token != null;
   }
@@ -42,6 +43,7 @@ class Auth with ChangeNotifier {
       _userId = responseBodyData['localId'];
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseBodyData['expiresIn'])));
+      _autoLogout();
       notifyListeners();
     } catch (err) {
       throw err;
@@ -62,6 +64,19 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
