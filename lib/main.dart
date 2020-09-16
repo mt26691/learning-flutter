7341d2c090b6a465f2f3ced867f921cc84a0e1f3
Tung Nguyen <nguyenmanhtung848@gmail.com>
2020-09-16 09:21:58 +0700

    set up firebase firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+bool USE_FIRESTORE_EMULATOR = false;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.settings = Settings(
+        host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  }
   runApp(MyApp());
 }
 
