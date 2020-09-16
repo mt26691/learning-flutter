1a4ec5c2b4eb30652f347f458671f38fb52fa81c
Tung Nguyen <nguyenmanhtung848@gmail.com>
2020-09-16 21:01:38 +0700

    add submit form function

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -6,6 +6,24 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    // remove all focus, so keyboard can be closed.
+    FocusScope.of(context).unfocus();
+    if (!isValid) {
+      return;
+    }
+    _formKey.currentState.save();
+    // User those value to send auth request to firebase
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,39 +33,79 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+                key: _formKey,
                 child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email Adress',
-                  ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                  ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                RaisedButton(
-                  onPressed: () {},
-                  child: Text('Login'),
-                ),
-                FlatButton(
-                    onPressed: () {},
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text('Create new account'))
-              ],
-            )),
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      key: ValueKey('email'),
+                      validator: (value) {
+                        if (value.isEmpty || !value.contains('@')) {
+                          return 'Please Enter a valid email address';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email Adress',
+                      ),
+                      onSaved: (newValue) {
+                        _userEmail = newValue;
+                      },
+                    ),
+                    if (!_isLogin)
+                      TextFormField(
+                        key: ValueKey('username'),
+                        validator: (value) {
+                          if (value.isEmpty || value.length < 4) {
+                            return 'Please Enter at least 4 characters';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                        onSaved: (newValue) {
+                          _userName = newValue;
+                        },
+                      ),
+                    TextFormField(
+                      key: ValueKey('password'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 7) {
+                          return 'Password must be at least 7 characters long';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      onSaved: (newValue) {
+                        _userPassword = newValue;
+                      },
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        _trySubmit();
+                      },
+                      child: Text(_isLogin ? 'Login' : 'Signup'),
+                    ),
+                    FlatButton(
+                        onPressed: () {
+                          setState(() {
+                            _isLogin = !_isLogin;
+                          });
+                        },
+                        textColor: Theme.of(context).primaryColor,
+                        child: _isLogin
+                            ? Text('Create new account')
+                            : Text('I already have an ccount'))
+                  ],
+                )),
           ),
         ),
       ),
